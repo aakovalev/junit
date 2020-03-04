42e8ddddbd1b23f0aa5497705db2402204eb8216
Alexey Kovalev <kovalev.aleksey@gmail.com>
2020-03-04 23:40:26 +0300

    small test refactoring

--- src/test/java/org/junit/custom/runners/RemoteRunListenerTest.java
+++ src/test/java/org/junit/custom/runners/RemoteRunListenerTest.java
@@ -8,30 +8,37 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import static org.junit.custom.runners.NotificationType.FireTestStarted;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
 public class RemoteRunListenerTest {
+
+    private static final int LISTENER_PORT = 1234;
+    private static final int TIMEOUT = 1000;
+
     @Test
     public void nothing() throws IOException {
-        int port = 1234;
         RunNotifier notifier = mock(RunNotifier.class);
-        RemoteRunListener listener = new RemoteRunListener(port, notifier);
+        RemoteRunListener listener = new RemoteRunListener(LISTENER_PORT, notifier);
         listener.start();
-        NotificationType type = NotificationType.FireTestStarted;
-        Description description = Description
-                .createSuiteDescription("Remote test started");
-        RunNotification notification =
-                new RunNotification(type, description);
-        send(notification, port);
-        verify(notifier, timeout(1000))
-                .fireTestStarted(eq(description));
+        RunNotification notification = createNotification(FireTestStarted);
+
+        send(notification, LISTENER_PORT);
+
+        verify(notifier, timeout(TIMEOUT))
+                .fireTestStarted(eq(notification.getDescription()));
     }
 
     private void send(RunNotification notification, int port) throws IOException {
         Socket client = new Socket("localhost", port);
-        ObjectOutputStream out =
-                new ObjectOutputStream(client.getOutputStream());
+        ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
         out.writeObject(notification);
+    }
+
+    private RunNotification createNotification(NotificationType type) {
+        Description description =
+                Description.createSuiteDescription("Remote test started");
+        return new RunNotification(type, description);
     }
 }
